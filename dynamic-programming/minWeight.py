d3cceeda24232061745831d06cf61cde870ad18f
YaoC <chengyao09@hotmail.com>
2016-11-02 21:01:19 +0800

    增加动态规划算法

--- dynamic-programming/minWeight.py
+++ dynamic-programming/minWeight.py
@@ -1,5 +1,9 @@
+import sys
+count = 0
 
 def minWeight(value,weight,totalValue):
+	global count 
+	count += 1
 	if totalValue == 0:
 		return 0
 	n = len(value)
@@ -14,6 +18,34 @@ def test_minWeight():
 	w = (1,2,4,6)
 	y = 12
 	print minWeight(v,w,y)
+	print count
+
+def minWeightDp(value,weight,totalValue):
+	coins = zip(value,weight)
+	minWeight = [0]
+	n = len(coins)
+	plan = [[]]
+	for t in xrange(1,totalValue+1):
+		weights = []
+		for coin in coins:
+			if coin[0] <= t:
+				weights.append(minWeight[t-coin[0]]+coin[1])
+			else:
+				weights.append(sys.maxint)
+		minW = min(weights)
+		idx = weights.index(minW)
+		plan.append(plan[t-coins[idx][0]]+[coins[idx][0]])
+		minWeight.append(minW)
+
+		
+	return plan[totalValue],minWeight[totalValue]
+
+def test_minWeightDp():
+	v = (1,4,6,8)
+	w = (1,2,4,6)
+	y = 12
+	print minWeightDp(v,w,y)
+
 
 if __name__ == '__main__':
-	test_minWeight()
+	test_minWeightDp()
